1a2c4891b2489922c93b27e42dc173e64df96150
subeen-me <subeen.me@gmail.com>
2022-04-10 22:22:20 +0900

    게시판 등록,수정, 삭제 기능 구현 완료

--- src/main/java/com/me/book/springboot/domain/posts/PostsService.java
+++ src/main/java/com/me/book/springboot/domain/posts/PostsService.java
@@ -1,11 +1,16 @@
 package com.me.book.springboot.domain.posts;
 
+import com.me.book.springboot.web.dto.PostsListResponseDto;
 import com.me.book.springboot.web.dto.PostsResponseDto;
 import com.me.book.springboot.web.dto.PostsSaveRequestDto;
 import com.me.book.springboot.web.dto.PostsUpdateRequestDto;
+import javafx.geometry.Pos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +38,22 @@ public class PostsService {
                 new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    /**
+     *postsRepository 결과로 넘어온 Posts의 stream을 map을 통해 PostsListResponseDto 변환 ->List로 반환하는 메소드
+     */
+    @Transactional(readOnly = true) //트랜잭션 범위는 유지하되, 조회 가능만 남겨두어 조회 속도가 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAlldesc().stream()
+                .map(PostsListResponseDto::new) // = .map(posts ->new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
     }
 }
